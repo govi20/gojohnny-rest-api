9d1e999129d717fdc140b630774a38bfb911ffca
yura-jt <cryobyte.zero@gmail.com>
2018-10-05 17:44:08 +0300

    5 added primary key to "place" creation script

--- src/main/resources/db/migration/V5_2_2018.10.06__Create_table_place.sql
+++ src/main/resources/db/migration/V5_2_2018.10.06__Create_table_place.sql
@@ -2,7 +2,9 @@ CREATE TABLE IF NOT EXISTS place (
  id BIGINT NOT NULL,
  name VARCHAR(255) NOT NULL ,
  altitude DECIMAL NOT NULL ,
- location GEOMETRY
+ location GEOMETRY,
+
+ CONSTRAINT place_pk PRIMARY KEY (id)
 );
 
 CREATE INDEX place_id_idx ON place (id);
