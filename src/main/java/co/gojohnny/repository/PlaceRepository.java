ac7eed54a8f2ee8a0a6e26ec32f99143a1b2c29e
tboychuk <tarass.boychuk@gmail.com>
2018-10-05 17:39:56 +0300

    #9 Create an endpoint to search list of places by url that contains ".../places/search/name/..."

--- src/main/java/co/gojohnny/repository/PlaceRepository.java
+++ src/main/java/co/gojohnny/repository/PlaceRepository.java
@@ -2,9 +2,17 @@ package co.gojohnny.repository;
 
 import co.gojohnny.model.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 @RepositoryRestResource
 public interface PlaceRepository extends JpaRepository<Place, Long> {
 
+    @RestResource(path = "/name")
+    @Query("select p from Place p where p.name like concat('%', :query, '%')")
+    List<Place> findPlacesByName(@Param("query") String query);
 }
