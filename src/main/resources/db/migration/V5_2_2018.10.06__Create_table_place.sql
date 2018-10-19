ea6283280cf202938c536767eee29299f3e3ac32
tboychuk <tarass.boychuk@gmail.com>
2018-10-19 07:36:32 +0300

    Change flyway migration scripts naming convention
    Issue: 42

--- src/main/resources/db/migration/V5_2_2018.10.06__Create_table_place.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS place (
- id BIGINT NOT NULL,
- name VARCHAR(255) NOT NULL ,
- altitude DECIMAL NOT NULL ,
- location GEOMETRY,
-
- CONSTRAINT place_pk PRIMARY KEY (id)
-);
